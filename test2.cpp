ec2977b1c03bb53f724a0e27d95969689e28dc2d
cdacasdm2019 <51844531+cdacasdm2019@users.noreply.github.com>
2019-06-15 11:13:51 +0530

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,9 +1,43 @@
-#include<iostream>
 
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
+  
 
-int main (){
+void insertionSort(int arr[], int n)  
+{  
+    int i, key, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1;  
+  
+    
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
+}  
+  
 
-cout<<"hello World";
-return 0;
-}
+void printArray(int arr[], int n)  
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        cout << arr[i] << " ";  
+    cout << endl; 
+}  
+  
+
+int main()  
+{  
+    int arr[] = { 12, 11, 13, 5, 6 };  
+    int n = sizeof(arr) / sizeof(arr[0]);  
+  
+    insertionSort(arr, n);  
+    printArray(arr, n);  
+  
+    return 0;  
+}  
